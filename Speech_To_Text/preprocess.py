9c5bae255fa221b8b1110847134aa6b55c86ea50
moazbk <moaz_bk@hotmail.com>
2020-08-28 15:48:16 +0200

    Speech Recognition System

---
+++ Speech_To_Text/preprocess.py
@@ -0,0 +1,77 @@
+import librosa
+import tensorflow as tf
+import os
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+import numpy as np
+from tqdm import tqdm
+
+DATA_PATH = "./data/"
+
+
+
+def get_labels(path=DATA_PATH):
+    labels = os.listdir(path)
+    label_indices = np.arange(0, len(labels))
+    return labels, label_indices, to_categorical(label_indices)
+
+
+# Handy function to convert from wave 2 mfcc
+def wav2mfcc(file_path, max_len=11):
+    wave, sr = librosa.load(file_path, mono=True, sr=None)
+    wave = wave[::3]
+    mfcc = librosa.feature.mfcc(wave, sr=16000)
+
+    # If maximum length exceeds mfcc lengths then exclude the remaining ones
+    if (max_len > mfcc.shape[1]):
+        pad_width = max_len - mfcc.shape[1]
+        mfcc = np.pad(mfcc, pad_width=(
+            (0, 0), (0, pad_width)), mode='constant')
+
+    # Else cutoff the remaining parts
+    else:
+        mfcc = mfcc[:, :max_len]
+
+    return mfcc
+
+
+def save_data_to_array(path=DATA_PATH, max_len=11):
+    labels, _, _ = get_labels(path)
+    # for each class extarct it's mfcc features
+    for label in labels:
+        # Inititalize the mfcc vectors
+        mfcc_vectors = []
+
+        wavfiles = [path + label + '/' +
+                    wavfile for wavfile in os.listdir(path + '/' + label)]
+        for wavfile in tqdm(wavfiles, "Saving vectors of label - '{}'".format(label)):
+            mfcc = wav2mfcc(wavfile, max_len=max_len)
+            mfcc_vectors.append(mfcc)
+        np.save(label + '.npy', mfcc_vectors)
+
+
+def get_train_test(split_ratio=0.7, random_state=42):
+    # Get all labels from the folder
+    labels, indices, _ = get_labels(DATA_PATH)
+    print(labels)
+    # Getting first arrays
+    X = np.load(labels[0] + '.npy')
+    y = np.zeros(X.shape[0])
+
+    # Append all of the dataset into one single array, same goes for y
+    for i, label in enumerate(labels[1:]):
+        x = np.load(label + '.npy')
+        X = np.vstack((X, x))
+        y = np.append(y, np.full(x.shape[0], fill_value=(i + 1)))
+
+    assert X.shape[0] == len(y)
+
+    return train_test_split(X, y, test_size=(1 - split_ratio), random_state=random_state, shuffle=True)
+
+def predict(filepath, model):
+    sample = wav2mfcc(filepath)
+    sample_reshaped = sample.reshape(1, feature_dim_1, feature_dim_2, channel)
+    return get_labels()[0][
+            np.argmax(model.predict(sample_reshaped))
+    ]
+
