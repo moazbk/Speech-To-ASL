5c18292b540fe075172683526ff08aff36c866d4
moazbk <moaz_bk@hotmail.com>
2020-08-28 15:52:04 +0200

    Speech Recognition System

---
+++ Avatar/Assets/Scripts/Listen.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections; 
+using System.Collections.Generic; 
+using System.Net; 
+using System.Net.Sockets; 
+using System.Text; 
+using System.Threading; 
+using UnityEngine;
+using UnityEngine.UI;
+public class Listen : MonoBehaviour
+{
+
+    #region private members
+    public bool changed;
+    public string mess;
+    private TcpListener tcpListener;
+    private string IP_Address;
+    private Thread tcpListenerThread;
+    	
+    private TcpClient connectedTcpClient;
+    #endregion
+    void Start()
+    {
+        this.IP_Address = GetLocalIPAddress();
+        changed = false;
+        // Start TcpServer background thread 		
+        tcpListenerThread = new Thread(new ThreadStart(ListenForIncommingRequests));
+        tcpListenerThread.IsBackground = true;
+        tcpListenerThread.Start();
+    }
+   string GetLocalIPAddress()
+    {
+        var host = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(changed)
+        {
+            FindObjectOfType<animate_script>().set_Text(mess);
+            changed = false;
+        }
+        
+    }
+
+    /// <summary> 	
+    /// Runs in background TcpServerThread; Handles incomming TcpClient requests 	
+    /// </summary> 	
+    private void ListenForIncommingRequests()
+    {
+        try
+        {
+            // Create listener on localhost port 8052. 			
+            tcpListener = new TcpListener(IPAddress.Parse(this.IP_Address), 8052);
+            tcpListener.Start();
+            Debug.Log("Server is listening");
+            Byte[] bytes = new Byte[1024];
+            while (true)
+            {
+                using (connectedTcpClient = tcpListener.AcceptTcpClient())
+                {
+                    // Get a stream object for reading 					
+                    using (NetworkStream stream = connectedTcpClient.GetStream())
+                    {
+                        int length;
+                        // Read incomming stream into byte arrary. 						
+                        while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                        {
+                            var incommingData = new byte[length];
+                            Array.Copy(bytes, 0, incommingData, 0, length);
+                            // Convert byte array to string message. 							
+                            string clientMessage = Encoding.ASCII.GetString(incommingData);
+                            mess = clientMessage;
+                            changed = true;
+                        }
+                    }
+                }
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("SocketException " + socketException.ToString());
+        }
+    }
+}
---
+++ Avatar/Assets/Scripts/animate_script.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class animate_script : MonoBehaviour {
+    private string all_message;
+    private string[] all_message_cutted;
+    private bool waits;
+    private bool anim_work;
+    private int counter;
+    private Animator anim;
+    public Text text_cutted;
+    public Text whole_text;
+    public float tim;
+    public bool start_cutting;
+	// Use this for initialization
+	void Start () {
+        start_cutting = false;
+        all_message="";
+        counter = 0;
+        waits = false;
+        this.anim_work = false;
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(start_cutting&&!this.waits)
+        {
+            words(counter);
+            StartCoroutine(waitTime());
+            counter++;
+        }
+	}
+    public void words(int i)
+    {
+        if (all_message_cutted.Length-1==i&&start_cutting)
+        {
+            start_cutting = false;
+            counter = 0;
+            text_cutted.text = "";
+        }
+        text_cutted.text = all_message_cutted[i];
+        whole_text.text = all_message;
+        Debug.Log(all_message_cutted[i]);
+        
+    }
+    public void set_Text(string all_text)
+    {
+        counter = 0;
+        all_message = all_text;
+        this.all_message_cutted = all_message.Split(' ');
+        start_cutting = true;
+    }
+    IEnumerator waitTime()
+    {
+        this.waits = true;
+            if (!anim_work)
+            {
+                this.anim_work = true;
+                anim.SetTrigger(text_cutted.text);
+            }
+        yield return new WaitForSeconds(this.tim);
+        anim.SetTrigger("idle");
+        this.anim_work = false;
+        this.waits = false;
+        
+    }
+}
